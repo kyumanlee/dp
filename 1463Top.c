f3de6f44a92d2d1fe27f66e7cc6f2d2c0a7422eb
kyumanlee <kmlee@konyang.ac.kr>
2017-10-15 16:34:20 +0900

    Update and rename 1464Top.c to 1463Top.c

--- 1463Top.c
+++ 1463Top.c
@@ -1,12 +1,3 @@
-/*
- ============================================================================
- Name        : dp1463Top.c
- Author      : 
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
- ============================================================================
- */
 
 #include <stdio.h>
 #include <stdlib.h>
